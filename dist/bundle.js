911f4041d6333f328f8cf14bf2d3f3e0a41cffa8
Luan Andrade <luanandrade.andrade@portoseguro.com.br>
2022-09-12 22:00:47 -0300

    config ambiente

---
+++ dist/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.jsx":
+/*!***********************!*\
+  !*** ./src/index.jsx ***!
+  \***********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nReferenceError: modele is not defined\n    at Object.<anonymous> (D:\\Estudos\\react\\01-github-explorer\\babel.config.js:1:1)\n    at Module._compile (node:internal/modules/cjs/loader:1103:14)\n    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1157:10)\n    at Module.load (node:internal/modules/cjs/loader:981:32)\n    at Function.Module._load (node:internal/modules/cjs/loader:822:12)\n    at Module.require (node:internal/modules/cjs/loader:1005:19)\n    at require (node:internal/modules/cjs/helpers:102:18)\n    at startHiding - secret - don't use this - v1 (D:\\Estudos\\react\\01-github-explorer\\node_modules\\@babel\\core\\lib\\errors\\rewrite-stack-trace.js:60:12)\n    at loadCjsDefault (D:\\Estudos\\react\\01-github-explorer\\node_modules\\@babel\\core\\lib\\config\\files\\module-types.js:104:69)\n    at loadCjsOrMjsDefault (D:\\Estudos\\react\\01-github-explorer\\node_modules\\@babel\\core\\lib\\config\\files\\module-types.js:76:16)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.jsx"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
